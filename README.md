600b00d82b5585bab9c58b93080542544ea16d9c
Hiei <blascogasconiban@gmail.com>
2017-03-09 17:46:56 +0100

    Ver:3.1/stable

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# README #
+ Name: creac
+ Author: Hiei <blascogasconiban@gmail.com>
+ Version: 3.1/stable
+ Description:
+              This Script will create file wherever you want with almost any extension
 
-Author: Hiei <blascogasconiban@gmail.com>
+ BUGS:         
+	      None found yet!
 
-Version: 2.7/stable
+ IMPORTANT:
+               -   If any extension is not included, you can add it manually by:
+                       1.- Create a file "1.extension" in the folder /etc/creac/ext
+                       2.- Add it to the case like (odt|xml|dtd)
 
-Summary: Create your pre-defined files wherever you want.
--- creac.sh
+++ creac.sh
@@ -1,65 +1,58 @@
 #!/bin/bash
 ##########################################################################################
-# Name: creac                                                                
-# Author: Hiei <blascogasconiban@gmail.com>                                                  
-# Version: 3.0b
-# Description: 
+# Name: creac
+# Author: Hiei <blascogasconiban@gmail.com>
+# Version: 3.1/stable
+# Description:
 #              This Script will create file wherever you want with almost any extension
-# 
+#
 # To do: Interactive if the file already exists
-#		
-#	                                                                                                                                            
-# IMPORTANT:                                                                             
-#	        -   If any extension is not included, you can add it manually by: 
+#
+# BUGS: 	None found yet!
+#
+# IMPORTANT:
+#	        -   If any extension is not included, you can add it manually by:
 #			1.- Create a file "1.extension" in the folder /etc/creac/ext
-#			2.- Add it to the case like ("odt")                                                                             
-#                                                                                                                                                    
+#			2.- Add it to the case like (odt|xml|dtd)
+#
 ##########################################################################################
-red=`tput setaf 1`
-reset=`tput sgr0`
-yellow=`tput setaf 3`
+bold=`tput bold`;red=`tput setaf 1`;reset=`tput sgr0`;yellow=`tput setaf 3` #colors
+#DEFAULT_CONFIG
+DIRECTORY=$(pwd)
+USER="" #modify the user in the getopts if you want other name
+FILES_DIRECTORY="/etc/creac/ext/"
 
+function create() {
+	mkdir -p "$3" #creates the directory if doesn't exist
+	cp -i "$4/1.$2" "$3" #copies the file
+	mv -i "$3/1.$2" "$3/$5$1.$2" #renames the file
+}
 
-ruta=`pwd`
-user="IvanBlasco_" #Modify the user if you want other name
+function help {
+	echo """${red}Usage: ${bold}[CREAC]${reset}${red} <OPTION>
+	OPTION:
+	    ${yellow}<noargs>    --> 	Creates the file in the active directory
+		<r>	--> 	Specify the route if you dont want the active directory
+		<d>	--> 	Creates the file inside the specified folder
+		<u>	-->	Creates the file with the USERNAME
+		<h>	--> 	Shows help${reset}"""
+	exit 0
+}
 
-if [[ "$1" == "-r" ]] || [[ "$1" == "--route" ]] || [[ "$1" == "-dr" ]] || [[ "$1" == "-rd" ]] ; then 
-	read -ep "${red}[CREAC]${yellow} Path: ${reset}" ruta #Changing "ruta" if -r
-	if [ ! -d $ruta ] && [[ "$1" == "-dr" ]] || [[ "$1" == "-rd" ]]  ; then
-  		mkdir -p $ruta;
-	fi
-fi
+while getopts ":r :d :u :h" OPT; do
+	case $OPT in
+		r) read -ep "${red}[CREAC]${yellow} Path: ${reset}" DIRECTORY ;;
+		d) read -p "${red}[CREAC]${yellow} Directory name: " FOLDER_NAME;DIRECTORY="$DIRECTORY/$FOLDER_NAME" ;;
+		u) USER="IvanBlasco_" ;;
+		h) help ;;
+	 	\?) echo "Invalid option: -$OPTARG";exit 1 ;;
+	esac
+done
 
-read -p ${red}"[CREAC]${yellow} Name of the file and the extension [file.ext]:${reset} " filename  #Reading the file name user wants
-
-ext=${filename##*.}		 #Cutting out the extension of the file
-name=${filename%"."$ext}	#Cutting out the name of the file
-
-case "$ext" in
-("odt"|"xml"|"dtd"|"dia"|"txt")		#Enabled extensions, just add the extension to the folder and add it for enable another one
-	
-	if [[ "$1" == "-d" ]] || [[ "$1" == "--directory" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]]  ; then 
-		mkdir -p "$ruta"/$name
-		if [[ ! -e "$ruta"/$name/"$user$name.$ext" ]] || [[ ! -e "$ruta"/$name/"$name.$ext" ]] ; then  
-			cp -i /etc/creac/ext/1.$ext "$ruta"/$name
-			if [[ "$1" == "-u" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]] ; then
-				mv -i "$ruta"/$name/1.$ext "$ruta"/$name/"$user$name.$ext"
-			else
-				mv -i "$ruta"/$name/1.$ext "$ruta"/$name/"$name.$ext"
-			fi
-		else
-			echo "File already exists"
-		fi
-	else
-		cp -i /etc/creac/ext/1.$ext "$ruta"
-		if [[ "$1" == "-u" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]] ; then
-			mv -i "$ruta"/1.$ext "$ruta"/"$user$name.$ext"
-		else
-			mv -i "$ruta"/1.$ext "$ruta"/"$name.$ext"
-		fi		
-	fi
-   ;;
-*) echo ${red}"The extension you chosen is not available at the moment!" ${reset}
-   ;;
+read -p "${red}[CREAC]${yellow} Extension for your file: ${reset}" FILE_EXTENSION
+case $FILE_EXTENSION in
+	odt|xml|dtd|dia|txt) read -p "${red}[CREAC]${yellow} Your file name: ${reset}" FILE_NAME
+			     create "$FILE_NAME" "$FILE_EXTENSION" "$DIRECTORY" "$FILES_DIRECTORY" "$USER" ;;
+	*) echo "${red}${bold}Error: choose a valid extension!${reset}";exit 1
 esac
 exit 0
