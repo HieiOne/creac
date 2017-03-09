da18b478977bea0e6b54d43ba97812facb17de67
Hiei <blascogasconiban@gmail.com>
2017-03-09 15:50:35 +0100

    Ver:2.7/stable

--- creac.sh
+++ creac.sh
@@ -2,9 +2,11 @@
 ##########################################################################################
 # Name: creac                                                                
 # Author: Hiei <blascogasconiban@gmail.com>                                                  
-# Version: 2.6                                                                           
+# Version: 2.7                                                                           
 # Description: 
 #              This Script will create file wherever you want with almost any extension
+# 
+# To do: Interactive if the file already exists
 #		
 #	                                                                                                                                            
 # IMPORTANT:                                                                             
@@ -16,37 +18,48 @@
 red=`tput setaf 1`
 reset=`tput sgr0`
 yellow=`tput setaf 3`
+
+
 ruta=`pwd`
 user="IvanBlasco_" #Modify the user if you want other name
-if [[ "$1" == "-r" ]] || [[ "$1" == "--route" ]] || [[ "$1" == "-dr" ]] || [[ "$1" == "-rd" ]] ; then
-	read -ep "${red}[CREAC]${yellow} Path: ${reset}" ruta
+
+if [[ "$1" == "-r" ]] || [[ "$1" == "--route" ]] || [[ "$1" == "-dr" ]] || [[ "$1" == "-rd" ]] ; then 
+	read -ep "${red}[CREAC]${yellow} Path: ${reset}" ruta #Changing "ruta" if -r
 	if [ ! -d $ruta ] && [[ "$1" == "-dr" ]] || [[ "$1" == "-rd" ]]  ; then
   		mkdir -p $ruta;
 	fi
 fi
-read -p ${red}"[CREAC]${yellow} Name of the file and the extension [file.ext]:${reset} " filename
-ext=${filename##*.}
-name=${filename%"."$ext}
+
+read -p ${red}"[CREAC]${yellow} Name of the file and the extension [file.ext]:${reset} " filename  #Reading the file name user wants
+
+ext=${filename##*.}		 #Cutting out the extension of the file
+name=${filename%"."$ext}	#Cutting out the name of the file
 
 case "$ext" in
-("odt"|"xml"|"dtd"|"dia"|"txt")
-	if [[ "$1" == "-d" ]] || [[ "$1" == "--directory" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]]  ; then
+("odt"|"xml"|"dtd"|"dia"|"txt")		#Enabled extensions, just add the extension to the folder and add it for enable another one
+	
+	if [[ "$1" == "-d" ]] || [[ "$1" == "--directory" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]]  ; then 
 		mkdir -p "$ruta"/$name
-		cp /etc/creac/ext/1.$ext "$ruta"/$name
-		if [[ "$1" == "-u" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]] ; then
-			mv "$ruta"/$name/1.$ext "$ruta"/$name/"$user$name.$ext"
+		if [[ ! -e "$ruta"/$name/"$user$name.$ext" ]] || [[ ! -e "$ruta"/$name/"$name.$ext" ]] ; then  
+			cp -i /etc/creac/ext/1.$ext "$ruta"/$name
+			if [[ "$1" == "-u" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]] ; then
+				mv -i "$ruta"/$name/1.$ext "$ruta"/$name/"$user$name.$ext"
+			else
+				mv -i "$ruta"/$name/1.$ext "$ruta"/$name/"$name.$ext"
+			fi
 		else
-			mv "$ruta"/$name/1.$ext "$ruta"/$name/"$name.$ext"
+			echo "File already exists"
 		fi
 	else
-		cp /etc/creac/ext/1.$ext "$ruta"
+		cp -i /etc/creac/ext/1.$ext "$ruta"
 		if [[ "$1" == "-u" ]] || [[ "$1" == "-ud" ]] || [[ "$1" == "-du" ]] ; then
-			mv "$ruta"/1.$ext "$ruta"/"$user$name.$ext"
+			mv -i "$ruta"/1.$ext "$ruta"/"$user$name.$ext"
 		else
-			mv "$ruta"/1.$ext "$ruta"/"$name.$ext"
+			mv -i "$ruta"/1.$ext "$ruta"/"$name.$ext"
 		fi		
 	fi
    ;;
 *) echo ${red}"The extension you chosen is not available at the moment!" ${reset}
    ;;
 esac
+exit 0
