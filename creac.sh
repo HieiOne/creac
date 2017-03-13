ffcfe83bedffe71616010749ffe35b8bee20ce2b
Hiei <blascogasconiban@gmail.com>
2017-03-13 15:10:38 +0100

    Ver:3.2/stable

--- creac.sh
+++ creac.sh
@@ -2,11 +2,11 @@
 ##########################################################################################
 # Name: creac
 # Author: Hiei <blascogasconiban@gmail.com>
-# Version: 3.1.1/stable
+# Version: 3.2/stable
 # Description:
 #              This Script will create file wherever you want with almost any extension
 #
-# To do: Interactive if the file already exists
+#
 #
 # BUGS: 	None found yet!
 #
@@ -17,10 +17,8 @@
 #
 ##########################################################################################
 bold=`tput bold`;red=`tput setaf 1`;reset=`tput sgr0`;yellow=`tput setaf 3` #colors
-#DEFAULT_CONFIG
-DIRECTORY=$(pwd)
-USER="" #modify the user in the getopts if you want other name
-FILES_DIRECTORY="" #add path where you stored your extensions
+DIRECTORY=$(pwd);USER=""
+source creac.conf
 
 function create() {
 	#$1=FILE_NAME $2=EXTENSION $3=DIRECTORY $4=FILES_DIRECTORY $5=USER
@@ -44,7 +42,7 @@ while getopts ":r :d :u :h" OPT; do
 	case $OPT in
 		r) read -ep "${red}[CREAC]${yellow} Path: ${reset}" DIRECTORY ;;
 		d) read -p "${red}[CREAC]${yellow} Directory name: " FOLDER_NAME;DIRECTORY="$DIRECTORY/$FOLDER_NAME" ;;
-		u) USER="$(whoami)_" ;; #default value user name
+		u) USER=$USER_NAME ;; #default value user name
 		h) help ;;
 	 	\?) echo "Invalid option: -$OPTARG";exit 1 ;;
 	esac
@@ -53,7 +51,7 @@ done
 read -p "${red}[CREAC]${yellow} Extension for your file: ${reset}" FILE_EXTENSION
 case $FILE_EXTENSION in
 	odt|xml|dtd|dia|txt) read -p "${red}[CREAC]${yellow} Your file name: ${reset}" FILE_NAME
-			     							create "$FILE_NAME" "$FILE_EXTENSION" "$DIRECTORY" "$FILES_DIRECTORY" "$USER" ;;
+ 			     create "$FILE_NAME" "$FILE_EXTENSION" "$DIRECTORY" "$FILES_DIRECTORY" "$USER" ;;
 	*) echo "${red}${bold}Error: choose a valid extension!${reset}";exit 1
 esac
 exit 0
